0b1416154f4c7233912157752e9d823257278322
madhava364 <madhavamarri28@gmail.com>
2026-05-03 19:45:42 +0530

    added category pages

--- app/10-mins-food/page.js
+++ app/10-mins-food/page.js
@@ -8,12 +8,3 @@ export const metadata = {
   description:
     "Get food delivered in 10 minutes in Bangalore using fast delivery apps.",
 };
-
-export default function Page() {
-  return (
-    <div>
-      <h1>10 Minute Food Delivery in Bangalore</h1>
-      <p>Explore ultra-fast food delivery services in Bangalore.</p>
-    </div>
-  );
-}
